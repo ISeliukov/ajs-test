1d4d0dfa62e435e778eb67aa19138fd97a9ff935
Garysi <garysi@rambler.ru>
2023-05-29 13:23:52 +0300

    fix 3 lint

--- src/JS/basic.js
+++ src/JS/basic.js
@@ -8,11 +8,14 @@ export default function sum(items) {
 
 export function health(obj) {
   if (obj.health > 50) return 'healthy';
-  else if (obj.health > 15) return 'wounded';
-  else return 'critical';
+  if (obj.health > 15) return 'wounded';
+  return 'critical';
 }
 
 export function sortHealth(arr) {
-  const arrsort = arr.sort((a, b) => { return (parseFloat(b.health) - parseFloat(a.health)); });
+  const arrsort = arr.sort((a, b) => {
+    const res = parseFloat(b.health) - parseFloat(a.health);
+    return res;
+  });
   return arrsort;
 }
